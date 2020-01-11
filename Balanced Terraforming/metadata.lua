25baf80956b21129b4d4f6a1a7348fd5f56db525
Lithovore <17753722+Lithovore@users.noreply.github.com>
2020-01-11 21:36:03 +1300

    Add files via upload

---
+++ Balanced Terraforming/metadata.lua
@@ -0,0 +1,18 @@
+return PlaceObj('ModDef', {
+	'title', "Balanced Terraforming",
+	'description', "This mod rebalances terraforming technologies, buildings, expeditions and crops, and makes terraforming paramaters interdependent. A denser atmosphere traps more heat; higher temperatures cause devolatilisation from the Martian regolith; plant growth depends on atmosphere, temperature and water; and vegetation contributes to atmospheric terraforming by converting CO2 into breathable oxygen. Terraforming elements have been rebalanced around these interdependencies",
+	'image', "Mars_terraforming-NASA.png",
+	'id', "ubJabHW",
+	'pops_desktop_uuid', "c17d64ec-7c7a-42a2-a8b0-16933f1b845c",
+	'pops_any_uuid', "da3503ba-4221-41a1-b7ca-de2e740d4f59",
+	'author', "Lithovore",
+	'version', 6,
+	'lua_revision', 233360,
+	'saved_with_revision', 249143,
+	'code', {
+		"Code/Script.lua",
+	},
+	'saved', 1578710590,
+	'screenshot1', "Mars_terraforming-NASA.png",
+	'TagGameplay', true,
+})
