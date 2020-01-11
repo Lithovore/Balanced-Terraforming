f6d70bf91f84fe1203435f43a3dd398e312ff9f2
Lithovore <17753722+Lithovore@users.noreply.github.com>
2020-01-11 21:52:00 +1300

    Update Script.lua

--- Balanced Terraforming/Code/Script.lua
+++ Balanced Terraforming/Code/Script.lua
@@ -19,12 +19,13 @@ end
 math.min = Min
 
 function OnMsg.NewHour(hour)
+
 	--Get current terraforming values
 	local currentAir = GetTerraformParam("Atmosphere")
 	local currentHeat = GetTerraformParam("Temperature")
 	local currentWater = GetTerraformParam("Water")
 	local currentPlants = GetTerraformParam("Vegetation")
-	
+	--Calculate changes
 	local airChange = floatfloor((currentPlants/2400)*(150000/(currentPlants+50000)))
 	local heatChange = floatfloor((currentAir*0.7-currentHeat)/480)
 	local waterChange = floatfloor((currentHeat*0.8-currentWater)/480)
@@ -33,8 +34,9 @@ function OnMsg.NewHour(hour)
 	if minEnv < currentPlants then
 		plantsChange = floatfloor((minEnv-currentPlants)/24)
 	elseif minEnv >= currentPlants then
-		plantsChange = floatfloor(((minEnv-currentPlants)*currentPlants/96000000)
+		plantsChange = floatfloor((minEnv-currentPlants)*currentPlants/96000000)
 	end
+	--Apply changes
 	ChangeTerraformParam("Atmosphere",airChange)
 	ChangeTerraformParam("Temperature",heatChange)
 	ChangeTerraformParam("Water",waterChange)
