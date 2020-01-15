1571a77b520ec8bfea3ddeb87b269c13dc532c0f
Lithovore <17753722+Lithovore@users.noreply.github.com>
2020-01-15 23:20:44 +1300

    Update ScriptTechs.lua

--- Balanced Terraforming/Code/ScriptTechs.lua
+++ Balanced Terraforming/Code/ScriptTechs.lua
@@ -24,6 +24,7 @@ TechFields.Terraforming.costs = {
 }
 
 TechDef.InterplanetaryProjects.position = range(1,1)
+TechDef.InterplanetaryProjects.description = T(713124966429, --[[ModItemTechPreset  description]] 'Unlocks the <color em>Import Atmospheric Gases</color> (improves Atmosphere <image UI/Icons/res_atmosphere.tga 1300>) and <color em>Capture Ice Asteroids</color> (improves Water <image UI/Icons/res_water_2.tga 1300>) special projects, accessible via the Planetary View.\n\n<color flavor>"If you want to lift yourself up, lift up someone else." \n<right>Booker T. Washington</color><left>'),
 TechDef.CarbonateProcessor.position = range(2,4)
 TechDef.CarbonateProcessor.description = T(102523438179, --[[ModItemTechPreset  description]] 'New Building: <color em>Carbonate Processor</color> (60<image UI/Icons/res_concrete.tga 1300> 20<image UI/Icons/res_metal.tga 1300> 10<image UI/Icons/res_machine_parts.tga 1300>) - improves the Atmosphere <image UI/Icons/res_atmosphere.tga 1300> of Mars by burning Waste Rock.\n\n<color flavor>"When fire is applied to a stone it cracks."</color>')
 TechDef.TerraformingRover.position = range(2,4)
